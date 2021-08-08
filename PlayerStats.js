68cb13be0525a786264acf217713cf624d4e27b3
Nitin Pasumarthy <nipasuma@linkedin.com>
2021-08-08 01:30:38 -0700

    Show game days by season

--- PlayerStats.js
+++ PlayerStats.js
@@ -35,9 +35,12 @@ function resolvePlayerName(player) {
 // find the most similar name to `player` in `knownPlayers` list
 // CHALLENGE: Harsha Nalajala uses Harsha as his name. Harsha Nannur used his full name. With this approach both will be merged!
 function findAliasIfAny(player, knownPlayers) {
+  const playerNameParts = player.split(/\s+/).map(p => p.trim())
   for(let kp of knownPlayers) {
-    if(player.indexOf(kp) >= 0 || kp.indexOf(player)) {
-      // a sub string of this player already exists
+    const kpNameParts = kp.split(/\s+/).map(p => p.trim())
+    // if either of first or last names match in any order, we treat both the names belong to the same person
+    const matchedNames = kpNameParts.filter(kpName => playerNameParts.filter(playerName => playerName.toLowerCase() === kpName.toLowerCase()).length > 0) // common names b/w given player and known players
+    if(matchedNames.length > 0) {
       return kp
     }
   }
@@ -80,7 +83,7 @@ function getPlayDaysPerPlayer() {
     for (let player of attendedPlayers) {
       player = resolvePlayerName(player);
       const gameTs = gameDate.getTime();
-      // player = findAliasIfAny(player, Object.keys(playerGameDays))
+      player = findAliasIfAny(player, Object.keys(playerGameDays))
       if (player in playerGameDays) {
         playerGameDays[player].push(gameTs)
       } else {
@@ -95,27 +98,39 @@ function getPlayDaysPerPlayer() {
 function getPlayCounts(season, useCache = true) {
   let playerGameDays = getPlayDaysPerPlayerFromCache() || {};
   let uniqGameDays = new Set();
+  const playersTally = [] // list is an easier format for the UI to process
 
   if (!useCache || Object.keys(playerGameDays).length == 0) {
     playerGameDays = getPlayDaysPerPlayer();
     cachePlayDaysPerPlayer(playerGameDays)
   }
-  const playersTally = [] // list is an easier format for the UI to process
-  if (season) {
-    // TODO
-  } else {
-    for (let player in playerGameDays) {
-      playersTally.push({ "name": player, "days": playerGameDays[player].length });
-      uniqGameDays = new Set([...uniqGameDays, ...playerGameDays[player]]) // add to the existing set, aka union
+
+  if (season && season > 0) {
+    const startTs = new Date(season, 0, 1).getTime();
+    const endTs = new Date(season + 1, 0, 1).getTime();
+    for(let player in playerGameDays) {
+      playerGameDays[player] = playerGameDays[player].filter(ts => ts >= startTs && ts <= endTs)
     }
   }
+
+  for (let player in playerGameDays) {
+    playersTally.push({ "name": player, "days": playerGameDays[player].length });
+    uniqGameDays = new Set([...uniqGameDays, ...playerGameDays[player]]) // add to the existing set, aka union
+  }
+  
   return { playersTally, "_meta": {
       "updatedAt": JSON.parse(db.getProperty(PLAYER_PLAY_DAYS_UPDATED_AT_KEY)),
       "totalGameDays": uniqGameDays.size
-    } }
+    } 
+  }
 }
 
 // ********* Tests **********
+
+function main() {
+  Logger.log(findAliasIfAny("Harsha", ["Harshavardhan Nannur", "harsha nalajala"]))
+}
+
 function testWriteDB() {
   db.setProperties({
     "one": JSON.stringify({ "q": [new Date().getTime()] }),
